2cebd0802046af2c460d2df23d609b1621b7dd48
Quentin Jallet <quentin@netby.io>
2023-09-24 14:53:10 +0200

    Adding event listener on left arrow and right arrow

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,28 @@
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
+    {
+        "image": "slide1.jpg",
+        "tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
+    },
+    {
+        "image": "slide2.jpg",
+        "tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+    },
+    {
+        "image": "slide3.jpg",
+        "tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
+    },
+    {
+        "image": "slide4.png",
+        "tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
+    }
 ]
+
+document.querySelector('.arrow_left').addEventListener('click', function (evt) {
+    evt.preventDefault();
+    console.log('click left');
+});
+
+document.querySelector('.arrow_right').addEventListener('click', function (evt) {
+    evt.preventDefault();
+    console.log('click right');
+});
